8f48008f8f77f663a23bdba88e39e4498ea9d8e0
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2026-03-09 21:44:53 +0000

    Update: Theme

--- init.lua
+++ init.lua
@@ -746,38 +746,12 @@ require('lazy').setup {
     },
   },
   {
-    'ellisonleao/gruvbox.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {},
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('gruvbox').setup {
-        styles = {
-          terminal_colors = true, -- add neovim terminal colors
-          undercurl = true,
-          underline = true,
-          bold = true,
-          italic = {
-            strings = true,
-            emphasis = true,
-            comments = true,
-            operators = false,
-            folds = true,
-          },
-          strikethrough = true,
-          invert_selection = false,
-          invert_signs = false,
-          invert_tabline = false,
-          inverse = true, -- invert background for search, diffs, statuslines and errors
-          contrast = '', -- can be "hard", "soft" or empty string
-          palette_overrides = {},
-          overrides = {},
-          dim_inactive = false,
-          transparent_mode = false,
-        },
-      }
-
-      vim.o.background = 'dark' -- or "light" for light mode
-      vim.cmd.colorscheme 'gruvbox'
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
